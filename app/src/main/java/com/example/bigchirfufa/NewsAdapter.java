65944d4e9935f574bc5a84456e39224c19f3d9d5
SomeOneWrite <csserf@mail.ru>
2018-06-10 23:06:45 +0500

    Добавлен функционал новостей

---
+++ app/src/main/java/com/example/bigchirfufa/NewsAdapter.java
@@ -0,0 +1,90 @@
+package com.example.bigchirfufa;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+    private ArrayList<Pair<String, String>> mData;
+    private LayoutInflater mInflater;
+
+
+
+    // data is passed into the constructor
+    NewsAdapter(Context context, ArrayList<Pair<String, String>> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.news_rec, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Pair<String, String> news = mData.get(position);
+        holder.myTextView.setText(news.first);
+        final ImageView img_view = holder.myImageView;
+        final ProgressBar progress_view = holder.myProgressBar;
+        Picasso.with(MainActivity.getAppContext()).load(news.second).into(holder.myImageView, new Callback() {
+            @Override
+            public void onSuccess() {
+
+                if ((progress_view != null) && (img_view != null))
+                {
+                    progress_view.setVisibility(View.GONE);
+                    img_view.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    throw new IllegalStateException("162: не удается найти картинку и/или прогресс бар!");
+                }
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView myTextView;
+        ImageView myImageView;
+        ProgressBar myProgressBar;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.news_text);
+            myImageView = itemView.findViewById(R.id.news_image);
+            myProgressBar = itemView.findViewById(R.id.progress_bar_news);
+        }
+
+    }
+}
